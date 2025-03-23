e2a8c59bb5cdd6c3f1131f61bf6b768b381db47b
SankaManka <margosem02@mail.ru>
2025-03-23 22:06:14 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Адрес бэка
+        target: 'http://localhost:8800', // Адрес бэка
         changeOrigin: true, // Меняет origin запроса (важно для CORS)
         secure: false, // Выключаем проверку SSL (если локально)
         ws: true,
