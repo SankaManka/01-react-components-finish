75ec1b89f7e7718244f900892e738e7e4331496d
SankaManka <margosem02@mail.ru>
2025-03-23 22:57:11 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://91.186.199.95:5173/', // Адрес бэка
-        changeOrigin: true, // Меняет origin запроса (важно для CORS)
-        secure: false, // Выключаем проверку SSL (если локально)
-        ws: true,
-        headers: {
-          'Access-Control-Allow-Origin': 'http://localhost:5173',
-        },
-      },
-    },
-    cors: true,
-  },
+  // Удалите весь блок server - он НЕ нужен для production!
+  base: '/', // Убедитесь, что base установлен корректно
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static'
+  }
 })
